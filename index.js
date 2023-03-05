520811bcff3013a7e59b12ab75007ea3f2efc2cf
EvgeniiaShirshikova <shirshikovae@gmail.com>
2023-03-05 14:39:26 -0600

    js

--- index.js
+++ index.js
@@ -2,17 +2,17 @@ const inpname = document.getElementById('inpname');
 const inpimg = document.getElementById('inpimg');
 const inpcomment = document.getElementById('inpcomment');
 const form = document.querySelector('form');
-const avatarimg = document.querySelector('.avatarimg');
+/* const avatarimg = document.querySelector('.avatarimg');
 const username = document.querySelector('.username');
-const comment = document.querySelector('.comment');
+const comment = document.querySelector('.comment'); */
+const chatarea = document.querySelector('.chatarea');
 
 
 form.addEventListener("submit", (event) => {
 event.preventDefault();
-avatarimg.src;
-avatarimg.src = inpimg.value;
-username.innerHTML = inpname.value;
-comment.innerHTML = inpcomment.value;
+
+chatarea.insertAdjacentHTML("beforeend", `<img class="avatarimg" src="${inpimg.value}" alt=""><div class="username">${inpname.value}</div><div class="comment">${inpcomment.value}</div>`);
+
 });
 
 
